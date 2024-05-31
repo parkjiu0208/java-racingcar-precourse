66fa85190645a48f72e0ef9a2213317fdb8980d3
박지우 <121562687+parkjiu0208@users.noreply.github.com>
2024-05-31 16:13:40 +0900

    Revert "Parkjiu0208"

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,7 @@
 package racingcar;
 
-import racingcar.controller.RacingController;
-
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
-        RacingController.main(args);
     }
 }
